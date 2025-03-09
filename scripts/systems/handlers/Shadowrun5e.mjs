8541ba977b61bc9627ad36411084caf4feb3eded
Thogrim1984 <github@thogrim.de>
2025-03-09 21:18:39 +0100

    Update for SR5 version 0.25.1

--- scripts/systems/handlers/Shadowrun5e.mjs
+++ scripts/systems/handlers/Shadowrun5e.mjs
@@ -1,6 +1,70 @@
 import {BaseSystem} from "../BaseSystem.mjs";
 import {settings} from "../../settings.mjs";
+import { BaseSystem } from "../BaseSystem.mjs";
+import { settings } from "../../settings.mjs";
 
+export class Shadowrun5e extends BaseSystem {
+  static system = 'shadowrun5e';
+
+  get sheetRenderHooks() {
+    const { render, rendered, onChange } = super.sheetRenderHooks;
+
+    return { render, rendered, onChange };
+  }
+
+  registerSettings(settingsData) {
+  }
+
+  registerSheetListeners() {
+  }
+
+  registerOther() {
+  }
+
+  registerHooks() {
+    Hooks.on("ready", () => {
+      console.log("Item Macro | Shadowrun5e System Initialized");
+    });
+
+    Hooks.on("SR5_PreActorItemRoll", (actor, item) => {
+      if (item.hasMacro() && settings.value("defaultmacro")) {
+        item.executeMacro();
+        return false;
+      } else {
+        return true;
+      }
+    });
+
+    Hooks.on("renderSR5BaseActorSheet", (actorsheet, html, data) => {
+      if (!settings.value("charsheet")) return true;
+
+      const form = html[0] || data.form;
+      const sheet = $(form);
+
+      sheet.find('.item-name').on('mousedown', (event) => {
+        event.preventDefault();
+        if(event.button === 2 && !settings.value("click")) return;
+        const iid = event.currentTarget.closest('.list-item').dataset.itemId; // from Helpers.listItemId(event);
+        const item = actorsheet.actor.items.get(iid);
+
+        if (item.hasMacro()) {
+          item.executeMacro();
+          return false;
+        } else {
+          return true;
+        }
+      });
+    });
+  }
+
+  systemValidation(macro) {
+    return true;
+  }
+
+  get itemTag() {
+    return '.item-name';
+  }
+}
 export class Shadowrun5e extends BaseSystem {
   static system = 'shadowrun5e';
 
